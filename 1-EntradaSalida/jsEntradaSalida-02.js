d61bc607ca0d040a5bfe15c434aa29ea6a9cae03
aobregon2501 <aobregon2501@gmail.com>
2022-01-31 20:20:43 -0300

    E/S 02

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,9 +1,14 @@
 /*
+Obregón Alex DIV I
+E/S 02
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	prompt(nombre);
-	alert(nombre);
+	var nombreTomado;
+
+	nombreTomado = prompt("Ingrese su nombre: ");
+
+	alert(nombreTomado);
 }
 
