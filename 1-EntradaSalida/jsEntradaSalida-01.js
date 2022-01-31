7c04ae8903800b73aab27a165270abf2d6661c15
aobregon2501 <aobregon2501@gmail.com>
2022-01-31 19:39:38 -0300

    E/S 01

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,11 @@
 /*
+
+Obregón Alex DIV I
+E/S 01
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola mundo");
+	alert("Esto funciona de maravilla");
 }
 
