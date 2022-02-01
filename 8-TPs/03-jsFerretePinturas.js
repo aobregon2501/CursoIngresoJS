47121adfe64c1c27c6a2034598b07382f06f3b6e
aobregon2501 <aobregon2501@gmail.com>
2022-02-01 19:03:58 -0300

    TP FP 03

--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -1,13 +1,30 @@
-/*3.	Para el departamento de Pinturas:
+/*
+Obregón Alex DIV I
+TP-FP 03
+3.	Para el departamento de Pinturas:
 A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
+
+var temperaturaIngresada;
+var cambioDeUnidad;
+
 function FahrenheitCentigrados () 
 {
-	
+	temperaturaIngresada = document.getElementById('txtIdTemperatura').value;
+
+    cambioDeUnidad = (temperaturaIngresada - 32) / 1.8;
+
+    alert(temperaturaIngresada + " grados fahrenheit equivalen a " + cambioDeUnidad + " grados centígrados");
 }
 
 function CentigradosFahrenheit () 
 {
-	
+	temperaturaIngresada = document.getElementById('txtIdTemperatura').value;
+
+    cambioDeUnidad = (temperaturaIngresada * 1.8) + 32;
+
+    alert(temperaturaIngresada + " grados centígrados equivalen a " + cambioDeUnidad + " grados fahrenheit");
 }
+
+// txtIdTemperatura
