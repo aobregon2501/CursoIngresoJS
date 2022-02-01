cd0e808ccec41baa4ea7a9f0423793dde2c29c6f
aobregon2501 <aobregon2501@gmail.com>
2022-02-01 17:52:17 -0300

    TP FF 01

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -1,17 +1,52 @@
-/*1.	Para el departamento de facturación:
+/*
+Obregón Alex DIV I
+TP-FF 01
+1.	Para el departamento de facturación:
 A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+
+var precioUno;
+var precioDos;
+var precioTres;
+
 function Sumar () 
 {
-	
+	precioUno = document.getElementById('txtIdPrecioUno').value;
+    precioDos = document.getElementById('txtIdPrecioDos').value;
+    precioTres = document.getElementById('txtIdPrecioTres').value;
+    var suma;
+
+    suma = parseInt(precioUno) + parseInt(precioDos) + parseInt(precioTres);
+
+    alert("La suma de los precios es " + suma);
 }
 function Promedio () 
 {
-	
+	precioUno = document.getElementById('txtIdPrecioUno').value;
+    precioDos = document.getElementById('txtIdPrecioDos').value;
+    precioTres = document.getElementById('txtIdPrecioTres').value;
+    var suma;
+    var promedio;
+
+    suma = parseInt(precioUno) + parseInt(precioDos) + parseInt(precioTres);
+    promedio = suma / 3;
+
+    alert("El promedio de los precios es " + promedio);
 }
 function PrecioFinal () 
 {
-	
+	precioUno = document.getElementById('txtIdPrecioUno').value;
+    precioDos = document.getElementById('txtIdPrecioDos').value;
+    precioTres = document.getElementById('txtIdPrecioTres').value;
+    var suma;
+    var precioFinal;
+
+    suma = parseInt(precioUno) + parseInt(precioDos) + parseInt(precioTres);
+    //precioFinal = suma + ...;
+
+    alert("El precio final es " + precioFinal);
 }
+
+// txtIdPrecioUno , txtIdPrecioDos , txtIdPrecioTres
