b88be5ba8d46320e28e04e09423627501fb26780
aobregon2501 <aobregon2501@gmail.com>
2022-01-31 22:25:00 -0300

    E/S 05

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -1,8 +1,17 @@
-/*
+/*Obregón Alex DIV I
+E/S 05
+
 Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	alert("ok");
+	var suNombre;
+	var suEdad;
+
+	suNombre = document.getElementById('txtIdNombre').value;
+	suEdad = document.getElementById('txtIdEdad').value;
+
+	alert("Su nombre es " + String(suNombre) + " y tiene " + String(suEdad) + " años.");
 }
 
+//nombre: txtIdNombre , edad: txtIdEdad
