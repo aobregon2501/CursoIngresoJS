f5fbaed7d73b0c17cbfbaa598ac23c3d530323d1
aobregon2501 <aobregon2501@gmail.com>
2022-01-31 21:46:46 -0300

    E/S 04

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -1,9 +1,16 @@
 /* 
+Obregón Alex DIV I
+E/S 04
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	var nombreUtilizado;
+
+	nombreUtilizado = prompt("Ingrese su nombre: ");
+
+	document.getElementById('txtIdNombre').value = nombreUtilizado;
 }
 
+//txtIdNombre
