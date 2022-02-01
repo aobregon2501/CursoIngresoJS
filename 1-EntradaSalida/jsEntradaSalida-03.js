969b1cadc94f76189fc6b6e165ea91379ef461dd
aobregon2501 <aobregon2501@gmail.com>
2022-01-31 21:21:30 -0300

    E/S 03

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,12 +1,15 @@
 /*
+Obregón Alex DIV I
+E/S 03
 Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	var suNombre;
+	
+	suNombre = document.getElementById('txtIdNombre').value;
 
+	alert(suNombre);
 }
 
-
+//txtIdNombre
